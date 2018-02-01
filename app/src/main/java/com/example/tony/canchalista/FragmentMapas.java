61814c0f1cace5e47dabbec1fd1d2ffda7b18345
Tony <tony81191@gmail.com>
2018-02-01 15:09:24 -0300

    se creo fragments y se hizo mapas

---
+++ app/src/main/java/com/example/tony/canchalista/FragmentMapas.java
@@ -0,0 +1,62 @@
+package com.example.tony.canchalista;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FragmentMapas extends SupportMapFragment implements OnMapReadyCallback {
+    private GoogleMap mMap;
+
+    public FragmentMapas() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View vista = inflater.inflate(R.layout.fragment_fragment_mapas, container, false);
+
+
+
+        return vista;
+    }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        Marcadores.complejos(mMap);
+
+
+        LatLng complejo = new LatLng(-26.8307052, -65.20279649999998);
+        mMap.addMarker(new MarkerOptions()
+                .position(complejo)
+                .title("Coplejo 1 - Tucuman - Argentina")
+                .snippet("Complejo 1 ")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE))
+        );
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(complejo));
+
+        // seteo mi posicion
+
+        //mMap.setMyLocationEnabled(true);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.getUiSettings().setCompassEnabled(true);
+        mMap.getUiSettings().setMapToolbarEnabled(true);
+        mMap.getUiSettings().setMyLocationButtonEnabled(true);
+
+        mMap.setMinZoomPreference(7.5f);
+        mMap.setMaxZoomPreference(19.0f);
+
+        mMap.setMapType(googleMap.MAP_TYPE_NORMAL);
+
+    }
+}
---
+++ app/src/main/java/com/example/tony/canchalista/Marcadores.java
@@ -0,0 +1,23 @@
+package com.example.tony.canchalista;
+
+/**
+ * Created by Tony on 1/2/2018.
+ */
+
+public abstract class Marcadores {
+    public static  void complejos(GoogleMap mMap){
+
+
+        LatLng complejo1 = new LatLng(-26.805079, -65.261692);//OZ Futbol	Avenida camino del Peru 1094
+        LatLng complejo2 = new LatLng(-26.854418, -65.224829);//Francia 98	Avenida alem 1950
+        LatLng complejo3 = new LatLng(-26.823316, -65.243055);//Alemania Padel & Club	Pasaje Alemania 67
+        LatLng complejo4 = new LatLng(-26.803357, -65.281589);//Unico Sport Futbol	Avenida Presidencia Peron 1100
+
+        mMap.addMarker(new MarkerOptions().position(complejo1).title("Coplejo 1").snippet("Complejo 1.1 ").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
+        mMap.addMarker(new MarkerOptions().position(complejo2).title("Coplejo 2").snippet("Complejo 2.2 ").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
+        mMap.addMarker(new MarkerOptions().position(complejo3).title("Coplejo 3").snippet("Complejo 3.3 ").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
+        mMap.addMarker(new MarkerOptions().position(complejo4).title("Coplejo 4").snippet("Complejo 4.4 ").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
+
+    }
+
+}
